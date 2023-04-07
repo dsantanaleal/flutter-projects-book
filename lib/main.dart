d6569cefacbab4fb06bc272439497953810db4e5
Diego Santana Leal <dsantanaleal@gmail.com>
2023-04-07 10:19:49 -0500

    Removing theme property and adding appbar color and text body color

--- lib/main.dart
+++ lib/main.dart
@@ -7,26 +7,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Hello World Travel Title",
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          titleTextStyle: TextStyle(
-            color: Colors.white,
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold
-          )
-        ),
-        textTheme: TextTheme(
-          titleLarge: TextStyle(
-          )
-        ),
-        colorScheme: ColorScheme.fromSwatch()
-      ),
       home: Scaffold(
         appBar: AppBar(
           title: Text("Hello World Travel Title"),
+          backgroundColor: Colors.deepPurple,
         ),
         body: Center(
-          child: Text("Hello World Travel"),
+          child: Text(
+            "Hello World Travel",
+            style: TextStyle(
+              fontSize: 26,
+              fontWeight: FontWeight.bold,
+              color: Colors.blue[800]
+            ),
+          )
         ),
       ),
     );
