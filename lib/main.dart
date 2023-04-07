40d73d186c324e652251672ffad1837774cf770d
Diego Santana Leal <dsantanaleal@gmail.com>
2023-04-07 14:31:32 -0500

    Adding AlertDialog with Contact Us action. Adding Padding to each main content

--- lib/main.dart
+++ lib/main.dart
@@ -12,37 +12,63 @@ class MyApp extends StatelessWidget {
           title: Text("Hello World Travel Title"),
           backgroundColor: Colors.deepPurple,
         ),
-        body: Center(
-          child: Column(
-            children: [
-              Text(
-                "Hello World Travel",
-                style: TextStyle(
-                    fontSize: 26,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blue[800]
-                ),
-              ),
-              Text(
-                "Discover the World",
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.deepPurpleAccent
-                )
-              ),
-              Image.network(
-                  'https://images.freeimages.com/images/large-previews/eaa/the-beach-1464354.jpg',
-                height: 350,
-              ),
-              ElevatedButton(
-                child: Text("Contact Us"),
-                onPressed: () => true,
+        body: Builder(
+          builder: (context) => Padding(
+            padding: EdgeInsets.all(20),
+            child: Center(
+              child: Column(
+                children: [
+                  Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Text("Hello World Travel", 
+                        style: TextStyle(
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue[800]
+                        ),
+                      )
+                  ),
+                  Padding(padding: EdgeInsets.all(5), child: Text(
+                    "Discover the World",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.deepPurpleAccent
+                    )
+                  )),
+                  Padding(padding: EdgeInsets.all(15), child: Image.network(
+                      'https://images.freeimages.com/images/large-previews/eaa/the-beach-1464354.jpg',
+                    height: 350,
+                  )),
+                  Padding(padding: EdgeInsets.all(15), child: ElevatedButton(
+                    child: Text("Contact Us"),
+                    onPressed: () => contactUs(context),
+                  ))
+                ],
               )
-            ],
+            ),
           )
-        ),
+        )
       ),
     );
   }
   
+}
+
+
+void contactUs(BuildContext context) {
+  showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Contact Us"),
+          content: Text("Mail us at hello@world.com"),
+          actions: [
+            TextButton(
+              child: Text("Close"),
+              onPressed: () => Navigator.of(context).pop(),
+            )
+          ],
+        );
+      }
+  );
 }
